e67bbf9e8c56b7ca6060d5b05a093b99775bcd9d
Tem <git@tem.zone>
2024-11-29 01:48:37 +0000

    feat: add some colors

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	let { children } = $props();
+</script>
+
+<style>
+    :global(:root) {
+        --primary: #ca93f7;
+        --secondary: #000000;
+        --background-color: rgb(211, 157, 255);
+        /* background-color: var(--primary); */
+        background-color: #000000;
+        text-align: center;
+        font-family: monospace;
+    }
+
+    :global(h1) {
+        /* font-weight: 500; */
+        letter-spacing: -1;
+        font-size: 70px;
+        margin-block: 0;
+    }
+    :global(body) {
+        margin: 0;
+    }
+</style>
+
+{@render children()}
